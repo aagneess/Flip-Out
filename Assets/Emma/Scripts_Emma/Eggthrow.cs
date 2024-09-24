b73ab94aced51997325235a838eefa62bb22ee2f
Emma <ewigur@gmail.com>
2024-09-24 10:13:47 +0200

    Update Eggthrow.cs

--- Assets/Emma/Scripts_Emma/Eggthrow.cs
+++ Assets/Emma/Scripts_Emma/Eggthrow.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 
 public class Eggthrow : MonoBehaviour
 {
-    public GameObject _egg;
-    public Transform ThrowPoint;
-    
-    public float ThrowRate = 1f;
-    public float throwIntervall = 1f;
+    [SerializeField] private GameObject _egg;
+    [SerializeField] private Transform ThrowPointStart;
+    [SerializeField] private Transform ThrowPointEnd;
+
+    private Vector3 centerPoint = new();
+
+    [SerializeField] float startTime;
+
+    [SerializeField] private float ThrowRate = 1f;
+
+    [SerializeField] private float throwJourney = 1f;
 
     void Update()
     {
@@ -18,13 +24,24 @@ public class Eggthrow : MonoBehaviour
 
     private void Trowing()
     {
+        centerPoint = (ThrowPointStart.position + ThrowPointEnd.position) * 0.5f;
+        
+        Vector3 relativeStart = ThrowPointStart.position - centerPoint;
+        Vector3 relativeEnd = ThrowPointEnd.position - centerPoint;
+
+
 
         //FireRate & Input
-        if (Input.GetKeyDown(KeyCode.Space) && Time.time > throwIntervall)
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > throwJourney)
         {
-            GameObject _throwInstance  = Instantiate(_egg, ThrowPoint.position, Quaternion.identity);
+            startTime = Time.time;
 
-            throwIntervall = Time.time + ThrowRate;
+            float fracTime = (Time.time - startTime) / throwJourney;
+
+            GameObject _throwInstance  = Instantiate(_egg);
+            transform.position = Vector3.Slerp(ThrowPointStart.position, ThrowPointEnd.position, fracTime);
+
+            throwJourney = Time.time + ThrowRate;
             
 
         }
