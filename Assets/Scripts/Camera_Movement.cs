91480080069f573e2946e0f812e4ce7406f14573
Spacestarz <samromell96@gmail.com>
2024-09-25 11:17:37 +0200

    fixed camera movement

--- Assets/Scripts/Camera_Movement.cs
+++ Assets/Scripts/Camera_Movement.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,20 +6,24 @@ using UnityEngine;
 public class Camera_Movement : MonoBehaviour
 {
     public Transform Player;
+    public float xlimit; 
 
+    //x limit för spelaran vad är positionen på kameran när den början 
+    //lägg den på efter camera.x 
     // Start is called before the first frame update
     void Start()
     {
-        
+        xlimit = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
-    {
-       
+    {  
         Vector3 Cameraposition = transform.position;
 
         Cameraposition.x = Player.position.x;
+
+        Cameraposition.x = Mathf.Max(Cameraposition.x, -xlimit); //takes  the largest value
 
         transform.position = Cameraposition;
     }
