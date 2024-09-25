5e0eb8e9c503b6112bbbd92d60d80bcedc5d3c41
Agnes Binett <agnesbinett@gmail.com>
2024-09-25 13:40:31 +0200

    Enemy update
    On fall, remove animator

--- Assets/Agnes Scripts/Enemy.cs
+++ Assets/Agnes Scripts/Enemy.cs
@@ -6,20 +6,22 @@ public class Enemy : MonoBehaviour
 {
 
     public Sprite flippedSprite;
-    public int health = 2;
+    public int health;
     
     private SpriteRenderer spriteRenderer;
     private BoxCollider2D boxCollider;
     private Rigidbody2D rb2D;
+    private Animator animator;
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         boxCollider = GetComponent<BoxCollider2D>();
         rb2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    public void Damage(int dmg) 
+    public void Damage(int dmg)
     {
         health -= dmg;
 
@@ -28,8 +30,9 @@ public class Enemy : MonoBehaviour
             FlipEnemy();
             RemoveBoxCollider();
             RemoveRigidBody();
-            rb2D.velocity = Vector2.zero;
-            Debug.Log(transform.position);
+            RemoveAnimator();
+            //rb2D.velocity = Vector2.zero;
+            //Debug.Log(transform.position);
         }
     }
 
@@ -40,18 +43,28 @@ public class Enemy : MonoBehaviour
 
     public void RemoveBoxCollider()
     {
-        if (boxCollider != null)
+        if (boxCollider)
         {
             Destroy(boxCollider);
             Debug.Log("BoxCollider2D removed");
         }
     }
+
     public void RemoveRigidBody()
     {
-        if (rb2D != null)
+        if (rb2D)
         {
             Destroy(rb2D);
-            Debug.Log("rb2D removed");
+            Debug.Log("Rigidbody removed");
+        }
+    }
+
+    public void RemoveAnimator()
+    {
+        if (animator)
+        {
+            Destroy(animator);
+            Debug.Log("Animator removed");
         }
     }
 }
