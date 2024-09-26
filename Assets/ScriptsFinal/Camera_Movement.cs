fa97fa6c5e8f7ef16b587f24efb437e774043bdf
Spacestarz <samromell96@gmail.com>
2024-09-26 09:19:50 +0200

    changes in camera movement script. Camera stop when get to end gameobject

--- Assets/ScriptsFinal/Camera_Movement.cs
+++ Assets/ScriptsFinal/Camera_Movement.cs
@@ -7,6 +7,7 @@ public class Camera_Movement : MonoBehaviour
 {
     public Transform Player;
     private float xlimit;
+    private bool canFollow = true; //check if camera can follow player
 
     //x limit för spelaran vad är positionen på kameran när den början 
     //lägg den på efter camera.x 
@@ -15,18 +16,31 @@ public class Camera_Movement : MonoBehaviour
     void Start()
     {
         xlimit = transform.position.x;
+        Debug.Log(canFollow);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 Cameraposition = transform.position;
+        if (canFollow)
+        {
+            Vector3 Cameraposition = transform.position;
+            Cameraposition.x = Player.position.x;
+            Cameraposition.x = Mathf.Max(Cameraposition.x, -xlimit); //takes  the largest value
+            transform.position = Cameraposition;
+        }
+    }
+    // Detect when the camera enters a trigger
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        // Check if the camera collided with the empty GameObject (tagged "StopCamera")
+        if (collider.gameObject.CompareTag("stopCamera"))
+        {
+            canFollow = false; // Stop the camera from following the player
+        }
 
-        Cameraposition.x = Player.position.x;
-
-        Cameraposition.x = Mathf.Max(Cameraposition.x, -xlimit); //takes  the largest value
-
-        transform.position = Cameraposition;
     }
 }
+ 
+
